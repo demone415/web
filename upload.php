f232381ef0f6b077c9ac8b029cc66fe867ebb750
demone415 <demone415@gmail.com>
2015-11-28 14:03:30 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -7,13 +7,13 @@ Result is "uploads/filename.extension" */
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-	//echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
-    //" has been uploaded";
+	echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
+    	" has been uploaded";
     chmod ("uploads/".basename( $_FILES['uploadedfile']['name']), 0644);
 	$file_path = $file_path . basename( $_FILES['uploadedfile']['name']);
-	echo $file_path;
+//	echo $file_path;
 	
-} else{
+} else {
     echo "There was an error uploading the file, please try again!";
     echo "filename: " .  basename( $_FILES['uploadedfile']['name']);
     echo "target_path: " .$target_path;
